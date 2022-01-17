63bc9ada776ce20df80b56ba25d5aad0d6ee1ac5
nothub <48992448+nothub@users.noreply.github.com>
2022-01-17 02:25:34 +0100

    init

---
+++ update.js
@@ -0,0 +1,21 @@
+const url = "https://api.github.com/repos/nothub/bitburner/contents/scripts";
+
+export async function main(ns) {
+
+    let response = await fetch(url).catch(error => ns.tprint("Fetch Error: " + error))
+    if (!response.ok) {
+        ns.tprint("Response Status: " + response.status + " " + response.statusText);
+        return;
+    }
+
+    let files = await response.json()
+    files.forEach(file => {
+        if (file.type === "file" && file.name.endsWith(".js")) {
+            ns.tprint(file.download_url)
+            // TODO: kill local file, delete local file, download remote file
+        } else {
+            ns.tprint("Skipping: " + file.name + " (" + file.type + ")")
+        }
+    })
+
+}
