62cce1e9d0d5e1ece6cdde126d9f5a2abe0ea12e
nothub <48992448+nothub@users.noreply.github.com>
2022-01-18 02:38:24 +0100

    use ns wget

--- update.js
+++ update.js
@@ -1,21 +1,24 @@
 const url = "https://api.github.com/repos/nothub/bitburner/contents/scripts";
 
 export async function main(ns) {
-
-    let response = await fetch(url).catch(error => ns.tprint("Fetch Error: " + error))
-    if (!response.ok) {
-        ns.tprint("Response Status: " + response.status + " " + response.statusText);
-        return;
-    }
-
-    let files = await response.json()
-    files.forEach(file => {
+    let files = await (await download(url)).json()
+    for (const file of files) {
         if (file.type === "file" && (file.name.endsWith(".script") || file.name.endsWith(".ns") || file.name.endsWith(".js"))) {
-            ns.tprint(file.download_url)
-            // TODO: kill local file, delete local file, download remote file
+            ns.tprint("Downloading: " + file.name)
+            await ns.kill(file.name, "home");
+            await ns.rm(file.name)
+            await ns.wget(file.download_url, file.name)
         } else {
             ns.tprint("Skipping: " + file.name + " (" + file.type + ")")
         }
-    })
+    }
+}
 
+async function download(url) {
+    let response = await fetch(url)
+    if (!response.ok) {
+        ns.tprint("Download error! Response status: " + response.status + " " + response.statusText);
+        exit()
+    }
+    return response
 }
