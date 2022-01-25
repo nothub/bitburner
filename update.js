e8e3a5f8d9d4a98e5abef34a125a2ff2ed56d6e2
nothub <48992448+nothub@users.noreply.github.com>
2022-01-25 14:48:12 +0100

    inline

--- update.js
+++ update.js
@@ -1,7 +1,12 @@
 const url = "https://api.github.com/repos/nothub/bitburner/contents/scripts";
 
 export async function main(ns) {
-    let files = await (await download(ns, url)).json()
+    let response = await fetch(url)
+    if (!response.ok) {
+        ns.tprint("Download error! Response status: " + response.status + " " + response.statusText);
+        ns.exit()
+    }
+    let files = await response.json()
     for (const file of files) {
         if (file.type === "file" && (file.name.endsWith(".script") || file.name.endsWith(".ns") || file.name.endsWith(".js"))) {
             ns.tprint("Downloading: " + file.name)
@@ -12,13 +17,4 @@ export async function main(ns) {
             ns.tprint("Skipping: " + file.name + " (" + file.type + ")")
         }
     }
-}
-
-async function download(ns, url) {
-    let response = await fetch(url)
-    if (!response.ok) {
-        ns.tprint("Download error! Response status: " + response.status + " " + response.statusText);
-        ns.exit()
-    }
-    return response
 }
