4f79a31ff2e158028204dabad67893abef8f158c
nothub <48992448+nothub@users.noreply.github.com>
2022-04-16 18:07:20 +0200

    manual-hack

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,22 @@
+const HOME = "home";
+
+const HACK_MANUAL = ["CSEC", "avmnite-02h"]
+
 export async function main(ns) {
 
-    let servers_known = ["home"];
+    if (ns.fileExists("Formulas.exe", HOME)) {
+        ns.tprint("formulas: " + Object.keys(ns.formulas))
+    }
+
+    await spawn_proc("upgrade-hacknet.script", HOME)
+
+    let servers_known = [HOME];
 
     let scanning = true
     while (scanning) {
         let countInitial = servers_known.length
         for (let server of servers_known) {
-            hack(server);
+            await hack(server);
             servers_known = servers_known.concat(ns.scan(server)
                 .filter(s => !servers_known.includes(s))
                 .filter(s => !ns.getPurchasedServers().includes(s)))
@@ -17,11 +27,21 @@ export async function main(ns) {
     }
 
     let servers_pwned = servers_known
-        .filter(s => s !== "home")
+        .filter(s => s !== HOME)
         .filter(s => ns.hasRootAccess(s))
 
     //ns.tprint("total network (" + servers_known.length + ") : " + servers_known)
     //ns.tprint("pwned targets (" + servers_pwned.length + ") : " + servers_pwned)
+
+    const spacers = servers_pwned.reduce((a, b) => a.length >= b.length ? a : b).length + 1
+
+    function spacer(host) {
+        let spacer = ""
+        for (let i = 0; i < spacers - host.length; i++) {
+            spacer = spacer + " "
+        }
+        return spacer
+    }
 
     for (let server of servers_pwned) {
         ns.killall(server)
@@ -30,38 +50,57 @@ export async function main(ns) {
         const threads = Math.trunc(ns.getServerMaxRam(server) / attack_memory)
         if (threads <= 0) continue
         for (let script of ["self-grow.script", "self-hack.script", "self-weak.script"]) {
-            await ns.scp(script, "home", server);
-            if (ns.exec(script, server, threads) === 0) {
-                ns.tprint("ERROR: pid 0 " + script + " on " + server + " @ " + threads + " threads")
-            }
+            await spawn_proc(script, server, threads)
         }
-        ns.tprint("utilized server: " + server + " (" + ns.getServerUsedRam(server) + "/" + ns.getServerMaxRam(server) + " GB @ " + threads + " threads)")
+        ns.tprint("self-pwn: " + server + spacer(server) + "(" + ns.getServerUsedRam(server) + "/" + ns.getServerMaxRam(server) + " GB @ " + threads + " threads)")
+    }
+
+    for (let server of HACK_MANUAL.filter(s => ns.hasRootAccess(s))) {
+        ns.tprint("manual-hack: " + server)
+        ns.connect(server)
+        await ns.singularity.manualHack()
+        ns.connect(HOME)
+    }
+
+    // TODO: purchase servers
+
+    for (let worker of ns.getPurchasedServers()) {
+        ns.tprint("worker: " + worker)
+        // TODO: link worker botnet
     }
 
     function hack(host) {
         let exploits = 0
-        if (ns.fileExists("BruteSSH.exe", "home")) {
+        if (ns.fileExists("BruteSSH.exe", HOME)) {
             ns.brutessh(host)
             exploits++
         }
-        if (ns.fileExists("FTPCrack.exe", "home")) {
+        if (ns.fileExists("FTPCrack.exe", HOME)) {
             ns.ftpcrack(host)
             exploits++
         }
-        if (ns.fileExists("RelaySMTP.exe", "home")) {
+        if (ns.fileExists("RelaySMTP.exe", HOME)) {
             ns.relaysmtp(host)
             exploits++
         }
-        if (ns.fileExists("HTTPWorm.exe", "home")) {
+        if (ns.fileExists("HTTPWorm.exe", HOME)) {
             ns.httpworm(host)
             exploits++
         }
-        if (ns.fileExists("SQLInject.exe", "home")) {
+        if (ns.fileExists("SQLInject.exe", HOME)) {
             ns.sqlinject(host)
             exploits++
         }
         if (exploits >= ns.getServerNumPortsRequired(host)) {
             ns.nuke(host)
+        }
+    }
+
+    async function spawn_proc(script, server, threads = 1) {
+        ns.kill(script, server)
+        if (server !== HOME) await ns.scp(script, HOME, server);
+        if (ns.exec(script, server, threads) === 0) {
+            ns.tprint("ERROR: pid 0 " + script + " on " + server + " @ " + threads + " threads")
         }
     }
 
