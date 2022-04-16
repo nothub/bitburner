f000f2a52ccaf4b5c2babc9d49ddfea7a68fcd9d
nothub <48992448+nothub@users.noreply.github.com>
2022-04-16 15:57:17 +0200

    ns2

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,41 @@
 export async function main(ns) {
 
-    let servers = ["home"];
+    let servers_known = ["home"];
 
     let scanning = true
     while (scanning) {
-        let countInitial = servers.length
-        for (let server of servers) {
+        let countInitial = servers_known.length
+        for (let server of servers_known) {
             hack(server);
-            servers = servers.concat(ns.scan(server)
-                .filter(s => !servers.includes(s))
+            servers_known = servers_known.concat(ns.scan(server)
+                .filter(s => !servers_known.includes(s))
                 .filter(s => !ns.getPurchasedServers().includes(s)))
         }
-        if (countInitial === servers.length) {
+        if (countInitial === servers_known.length) {
             scanning = false
-            ns.tprint("total network: " + servers.length + " -> " + servers)
-            ns.tprint("pwned targets: " + servers.filter(s => ns.hasRootAccess(s)).length + " -> " + servers.filter(s => ns.hasRootAccess(s)))
         }
+    }
+
+    let servers_pwned = servers_known
+        .filter(s => s !== "home")
+        .filter(s => ns.hasRootAccess(s))
+
+    //ns.tprint("total network (" + servers_known.length + ") : " + servers_known)
+    //ns.tprint("pwned targets (" + servers_pwned.length + ") : " + servers_pwned)
+
+    for (let server of servers_pwned) {
+        ns.killall(server)
+        let threads = Math.trunc(ns.getServerMaxRam(server) / (0.18 + 0.18 + 0.175))
+        threads = Math.trunc(ns.getServerMaxRam(server) / 5.35) // TODO: fix thread calculation
+        if (threads <= 0) continue
+        await ns.scp(["self-grow.script", "self-hack.script", "self-weak.script"], "home", server);
+        const process_id_grow = ns.exec("self-grow.script", server, threads)
+        if (process_id_grow === 0) ns.tprint("failed starting self-grow.script on " + server)
+        const process_id_hack = ns.exec("self-hack.script", server, threads)
+        if (process_id_hack === 0) ns.tprint("failed starting self-hack.script on " + server)
+        const process_id_weaken = ns.exec("self-weak.script", server, threads)
+        if (process_id_weaken === 0) ns.tprint("failed starting self-weak.script on " + server)
+        ns.tprint("utilized server: " + server + " (" + ns.getServerUsedRam(server) + "/" + ns.getServerMaxRam(server) + " GB @ " + threads + " threads)")
     }
 
     function hack(host) {
