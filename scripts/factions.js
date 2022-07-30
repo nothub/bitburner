84a2f6720620c5a2d8b95fc117da23717a047305
hub <48992448+nothub@users.noreply.github.com>
2022-07-30 14:41:27 +0200

    print faction augs

---
+++ scripts/factions.js
@@ -0,0 +1,47 @@
+const all_factions = [
+    "Aevum",
+    "Bachman & Associates",
+    "BitRunners",
+    "Blade Industries",
+    "Bladeburners",
+    "Chongqing",
+    "Church of the Machine God",
+    "Clarke Incorporated",
+    "CyberSec",
+    "Daedalus",
+    "ECorp",
+    "Four Sigma",
+    "Fulcrum Secret Technologies",
+    "Illuminati",
+    "Ishima",
+    "KuaiGong International",
+    "MegaCorp",
+    "NWO",
+    "Netburners",
+    "New Tokyo",
+    "NiteSec",
+    "OmniTek Incorporated",
+    "Sector-12",
+    "Shadows of Anarchy",
+    "Silhouette",
+    "Slum Snakes",
+    "Speakers for the Dead",
+    "Tetrads",
+    "The Black Hand",
+    "The Covenant",
+    "The Dark Army",
+    "The Syndicate",
+    "Tian Di Hui",
+    "Volhaven"
+];
+
+export async function main(ns) {
+    all_factions.forEach(faction => {
+        ns.tprint(faction)
+        ns.getAugmentationsFromFaction(faction).forEach(augment => {
+            if (!ns.getOwnedAugmentations(true).includes(augment)) {
+                ns.tprint(" - " + augment)
+            }
+        })
+    })
+}
